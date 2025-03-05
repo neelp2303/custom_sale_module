eac5efada3545931d3d32fd366df11bf4b23ae78
neelp2303 <neelp6475@gmail.com>
2025-03-05 13:01:36 +0530

    new file:   __init__.py 	new file:   __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "BizzAppDev Sale Customization",
+    "description": """Custom Sale Module by Neel Patel
+    - Enhances Sale, Delivery, and Purchase Orders
+    - Implements advanced partner search and tagging features
+    - Restricts modifications post-confirmation
+    """,
+    "summary": "Enhancements for Sales, Deliveries, and Purchases",
+    "version": "1.0.0",
+    "category": "Sales",
+    "sequence": 1,
+    "author": "Neel Patel",
+    "depends": ["sale", "stock", "purchase"],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
